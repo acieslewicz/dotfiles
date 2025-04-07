c8d0810df8488ef5f3f6d97b3bf2978d257cb7a3
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2025-04-07 17:14:26 -0600

    refactor: Relocate script filtering to chezmoiignore
    No longer need to make every powershell script a template chezmoi will
    simply ignore windows script folder on non windows systems

--- .chezmoiscripts/windows/run_onchange_install-packages.ps1
+++ .chezmoiscripts/windows/run_onchange_install-packages.ps1
@@ -1,4 +1,3 @@
-{{ if eq .chezmoi.os "windows" -}}
 # Self-elevate the script if required
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
   if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
@@ -15,4 +14,3 @@ if (Test-Path $configPath) {
 } else {
     Write-Error "Configuration file not found at $configPath"
 }
-{{ end -}}
