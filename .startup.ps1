43454495ee96ab3cb73d09afbb5ded315bb18154
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2025-04-07 18:14:39 -0600

    fix: Reload path after chezmoi install

--- .startup.ps1
+++ .startup.ps1
@@ -1,3 +1,6 @@
 winget install twpayne.chezmoi --source winget
 
+# Load updated paths to avoid restarting shell
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
 chezmoi init https://github.com/acieslewicz/dotfiles
