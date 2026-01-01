52745d17e0e67c3727e6afb5e5a763c74ec74cbd
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2026-01-01 12:33:53 -0700

    Import correct module as workaround for broken module imports

--- .chezmoiscripts/windows/run_windows_terminal.ps1
+++ .chezmoiscripts/windows/run_windows_terminal.ps1
@@ -6,7 +6,7 @@ if (-not (Test-Path (Split-Path $dest))) { exit 0 }
 if ((Get-Item $dest -ErrorAction SilentlyContinue).LinkType -eq 'SymbolicLink') { exit 0 }
 
 if ((Test-Path $src) -and (Test-Path $dest)) {
-    Import-Module Microsoft.PowerShell.Utility
+    Import-Module $PSHOME\Modules\Microsoft.PowerShell.Utility -Function Get-FileHash
     if ((Get-FileHash $src).Hash -eq (Get-FileHash $dest).Hash) { exit 0 }
 }
 
